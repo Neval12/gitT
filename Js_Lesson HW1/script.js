31e549a8aa8aa55547db7f34671625b1b561e4ee
Neval12 <57132086+Neval12@users.noreply.github.com>
2020-01-17 18:14:32 +0300

    Add files via upload

---
+++ Js_Lesson HW1/script.js
@@ -0,0 +1,13 @@
+(function)
+
+var div = document.getElementsById('test');
+    style = div.style;
+//     style.color = "red";
+//     style.backgroundColor = "black";
+//     style.border = "1px solid blue";
+//     style.padding = "3px";
+// console.log( div );
+
+div.className = " css-class  css-class-new ";
+
+;
